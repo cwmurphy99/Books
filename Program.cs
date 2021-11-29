9e5b2d75621e16cc9072254779f7236d85902faa
Chris Murphy <theworldisyours@gmail.com>
2021-11-29 14:21:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book()
+            {
+                Name = "Watchmen",
+                Author = "Alan Moore"
+            };
+
+            book.SetBookStatus();
+
+            Console.WriteLine(book.Name + " " + book.Author);
+            Console.WriteLine(book.IsCheckedOut);
+        }
+    }
+}
